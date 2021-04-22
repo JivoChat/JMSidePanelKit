e3a55aefb5703375620b0ada2f69857d0e7b522b
Anton_Karpushko <habrabro@gmail.com>
2021-04-22 14:25:18 +0300

    Initial commit!

---
+++ JMSidePanelController.h
@@ -0,0 +1,19 @@
+//
+//  JMSidePanelController.h
+//  JMSidePanelController
+//
+//  Created by Stan Potemkin on 01/11/2018.
+//  Copyright © 2018 JivoSite. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JMSidePanelController.
+FOUNDATION_EXPORT double JMSidePanelControllerVersionNumber;
+
+//! Project version string for JMSidePanelController.
+FOUNDATION_EXPORT const unsigned char JMSidePanelControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JMSidePanelController/PublicHeader.h>
+
+
